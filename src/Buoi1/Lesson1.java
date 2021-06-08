615fa62450500605b4fd0839c4c4545cdc9b96a1
= <=>
2021-06-08 11:22:55 +0700

    đẩy file bài tập buổi 1

---
+++ src/Buoi1/Lesson1.java
@@ -0,0 +1,37 @@
+package Buoi1;
+
+import java.util.Scanner;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.print("Nhập a: ");
+        int a= scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Nhập b: ");
+        int b=scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Nhập c: ");
+        int c=scanner.nextInt();
+        scanner.nextLine();
+        int maxNumber,minNumber;
+        maxNumber=minNumber=a;
+        if (b>maxNumber){
+
+            maxNumber=b;
+        }
+        if (c>maxNumber){
+            maxNumber=c;
+        }
+        System.out.println("maxNumber: "+maxNumber);
+
+        if (b<minNumber){
+            minNumber=b;
+        }
+        if (c<minNumber){
+            minNumber=c;
+        }
+        System.out.println("minNumber: "+minNumber);
+    }
+}
